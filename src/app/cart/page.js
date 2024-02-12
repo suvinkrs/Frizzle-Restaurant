cfc03cbc438b29bca7467d09b0488a2e7bc81734
Suvin <suvinkrsingh@gmail.com>
2024-02-12 16:51:11 +0530

    key 1

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -93,6 +93,7 @@ export default function CartPage() {
           )}
           {cartProducts?.length > 0 && cartProducts.map((product, index) => (
             <CartProduct
+              key={index}
               index={index}
               product={product}
               onRemove={removeCartProduct}
