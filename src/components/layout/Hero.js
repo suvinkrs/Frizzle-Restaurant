3b0dfb03838ba463c98d76556a37f0dac0b3c69f
Suvin <suvinkrsingh@gmail.com>
2024-02-18 00:39:24 +0530

    links fixed

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,6 +1,6 @@
 import Right from "@/components/icons/Right";
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Hero() {
   return (
     <section className="hero md:mt-4">
@@ -14,15 +14,15 @@ export default function Hero() {
           </span>
         </h1>
         <p className="my-6 text-gray-500 text-sm">
-        Indulge in irresistible fast food flavors, satisfying every craving with every bite, a taste sensation awaits you!
+          Indulge in irresistible fast food flavors, satisfying every craving with every bite, a taste sensation awaits you!
         </p>
         <div className="flex gap-4 text-sm">
           <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
-            Order now
+            <Link href={'/menu'}>Order now</Link>
             <Right />
           </button>
           <button className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold">
-            Learn more
+            <Link href={'/#about'}>Learn more</Link>
             <Right />
           </button>
         </div>
