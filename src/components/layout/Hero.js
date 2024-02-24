8bda108037a00a7eec913e56ea24a9642f3808de
Suvin <suvinkrsingh@gmail.com>
2024-02-24 14:27:07 +0530

    hero

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Hero() {
   return (
-    <section className="hero md:mt-4">
+    <section className="hero md:mt-4 bg-[#5AC0DF] rounded-lg p-3 mt-4">
       <div className="py-8 md:py-12">
         <h1 className="text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-black via-red-800 to-red-500">
           Everything<br />
@@ -14,18 +14,18 @@ export default function Hero() {
           Indulge in irresistible fast food flavors, satisfying every craving with every bite, a taste sensation awaits you!
         </p> */}
         <div className="flex pt-9 gap-4 text-sm">
-          <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
+          <button className="flex justify-center bg-primary uppercase items-center gap-2 text-white px-4 py-2 rounded-full border-gray-800 border-spacing-0  hover:bg-black hover:border-black hover:text-white transition-colors duration-500">
             <Link href={'/menu'}>Order now</Link>
             <Right />
           </button>
-          <button className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold">
+          <button className="flex items-center  text-gray-800 font-semibold border-gray-800 rounded-full border-spacing-0  hover:bg-black hover:text-white transition-colors duration-500">
             <Link href={'/#about'}>Learn more</Link>
             <Right />
           </button>
         </div>
       </div>
       <div className="relative hidden md:block">
-        <Image src={'/chicken.png'} layout={'fill'} objectFit={'contain'} alt={'chicken'} />
+        <Image src={'/fast food-pana.png'} layout={'fill'} objectFit={'contain'} alt={'chicken'} />
       </div>
     </section>
   );
